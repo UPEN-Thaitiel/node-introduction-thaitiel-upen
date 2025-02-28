64a5b932f2f563ed1475196f66e9757f5443fc1b
thaitiel-upen <thaitiel.duran@upnay.edu.mx>
2025-02-27 23:14:26 -0600

    Add files via upload

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,5 @@
+// import fs module
+
+//Using fs module create a file called message.txt and write a message in there
+
+//Using fs module read the  file creatd and ans show the output in the terminal
---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+//Initialize NPM with npm init command
+
+// Install sillyname package with npm install
+
+// Use sillyname package to print a random silly name
+
+// Install superheroes package with npm install
+
+// Use superheroes package to print a random silly name
